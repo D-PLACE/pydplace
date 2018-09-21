41022afe7c232257491c67f84dcee2bba18fdc99
xrotwang <xrotwang@googlemail.com>
2018-09-21 08:49:16 +0200

    release 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydplace',
-    version='0.5.0.dev0',
+    version='0.5.0',
     license='Apache 2.0',
     description='programmatic access to D-PLACE/dplace-data',
     long_description=open('README.md').read(),

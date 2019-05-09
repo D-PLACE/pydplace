06fbd82302a2c85ff6cd22d68f5cc36d772e1314
xrotwang <xrotwang@googlemail.com>
2019-05-09 21:48:34 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydplace',
-    version='2.1.0',
+    version='2.1.1.dev0',
     license='Apache 2.0',
     description='programmatic access to D-PLACE/dplace-data',
     long_description=open('README.md').read(),

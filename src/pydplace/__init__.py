710271f2d7e5eee3ef381c40da8297f43ce0cdf1
Robert Forkel <xrotwang@googlemail.com>
2024-08-14 10:12:00 +0200

    release 3.1.0

--- src/pydplace/__init__.py
+++ src/pydplace/__init__.py
@@ -1,6 +1,6 @@
 #
 from .dataset import DatasetWithSocieties, DatasetWithoutSocieties
 
-__version__ = "3.0.1.dev0"
+__version__ = "3.1.0"
 assert DatasetWithSocieties
 assert DatasetWithoutSocieties
